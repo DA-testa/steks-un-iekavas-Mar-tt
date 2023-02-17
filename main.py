2b41199ccbf41fff56e612812811f7d14d1d6fb2
Mar_tt <63107828+Mar-tt@users.noreply.github.com>
2023-02-17 10:22:42 +0000

    commit main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def find_mismatch(text):
           else: return i+1
         if len(opening_brackets_stack)==0:
             return -2;
-        else: return len(text)
+        # else: return len(text)
         
           
           
