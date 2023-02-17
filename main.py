c219d9115f6832d7dcb6cab1199a08472202bca4
Mar_tt <63107828+Mar-tt@users.noreply.github.com>
2023-02-17 10:47:30 +0000

    commit main.py

--- main.py
+++ main.py
@@ -28,10 +28,11 @@ def find_mismatch(text):
               skobka = "{"
           if (len(opening_brackets_stack)>0) and (len(opening_brackets_stack)-1 == skobka):
               opening_brackets_stack.pop()
-          else: return i+1
+          else: 
+            return i+1
         if (len(opening_brackets_stack) == 0):
             return -2;
-        else: return len(text)
+        # else: return len(text)
         
           
           
