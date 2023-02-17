65652d00b0bae15dd0a06c4a2cff88c78011720c
Mar_tt <63107828+Mar-tt@users.noreply.github.com>
2023-02-17 10:35:09 +0000

    commit main.py

--- main.py
+++ main.py
@@ -26,11 +26,15 @@ def find_mismatch(text):
               skobka = "["
             case 2:
               skobka = "{"
-          if (len(opening_brackets_stack)>0) and (len(opening_brackets_stack)-1 == skobka):
-              opening_brackets_stack.pop()
-          else: return i+1
-        if len(opening_brackets_stack)==0:
+          if (len(opening_brackets_stack)>0):
+            if (len(opening_brackets_stack)-1 == skobka):
+                opening_brackets_stack.pop()
+            else:
+                return i+1
+
+          if (len(opening_brackets_stack)==0):
             return -2;
+
         # else: return len(text)
         
           
