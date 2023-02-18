47b9c71eaf28db06ea518b64be97ede52add9c3f
Mar_tt <63107828+Mar-tt@users.noreply.github.com>
2023-02-18 09:57:01 +0000

    commit main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def find_mismatch(text):
                     skobka = "["
                 case 2:
                     skobka = "{"
-            if (len(opening_brackets_stack)>0) and (opening_brackets_stack[len(opening_brackets_stack)-1] == skobka):
+            if (len(opening_brackets_stack)>0) and (opening_brackets_stack.top() == skobka):
                 opening_brackets_stack.pop()
             else: 
                 return i+1
