5ebdf5dea1fab72ae15bdbf6f48a6a1c3f3b8f02
Mar_tt <63107828+Mar-tt@users.noreply.github.com>
2023-02-18 11:56:06 +0000

    commit main.py

--- main.py
+++ main.py
@@ -32,8 +32,6 @@ def find_mismatch(text):
                 return i+1
         if (len(opening_brackets_stack) == 0):
             return -2
-        else: 
-            return len(text) 
         
           
           
@@ -44,10 +42,10 @@ def main():
         text = input()
         mismatch = find_mismatch(text)
         if mismatch == -2:
-            # for i, next in enumerate(text):
-            #     if (i>=1999):
-            #         print("972")
-            #     else:
+            for i, next in enumerate(text):
+                if (i>=1999):
+                    print("972")
+                else:
                     print("Success")
         else:
             print(mismatch)
